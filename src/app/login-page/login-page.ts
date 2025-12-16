e0698e7f00ede9fc03e578132dbec255c01c890c
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-16 18:10:44 +0100

    feat : add toggle password function for hidde/view password in the psw input

--- src/app/login-page/login-page.ts
+++ src/app/login-page/login-page.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+  form;
+  showPassword = false;
+  
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
 
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
