8e95058b9392e9c522e4b9b78b9f48eea0907a3e
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-19 11:27:39 +0100

    Ensure form validation

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+const baseUrl = 'http://localhost:8989/api';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthService {
 
+  constructor(private http: HttpClient) {}
+
+  authenticateUser(email: string, password: string): Observable<any> {
+    const url = `${baseUrl}/auth/signin`;
+    return this.http.post(url, { email, password }, httpOptions);
+  }
 }
