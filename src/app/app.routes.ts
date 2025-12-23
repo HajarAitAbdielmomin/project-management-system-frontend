30e2f9fff0eb28328f5aaa225796f35afa6de0f7
Hajar ait abdielmomin <60707026+Hajar20@users.noreply.github.com>
2025-12-23 20:53:54 +0100

    add more routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
 import {LoginPage} from './auth/login-page/login-page';
-
+import {Dashboard} from './users/admin/dashboard/dashboard';
+import {Dashboard as DashboardPO} from './users/product-owner/dashboard/dashboard';
+import {Dashboard as DashboardPM} from './users/project-manager/dashboard/dashboard';
+import {Dashboard as DashboardTM} from './users/team-member/dashboard/dashboard';
 export const routes: Routes = [
   {
     path: '',
     component: LoginPage,
-  }
+  },
+  {
+    path: 'admin/dashboard',
+    component: Dashboard
+  },
+  {
+    path: 'product-owner/dashboard',
+    component: DashboardPO
+  },
+  {
+    path: 'project-manager/dashboard',
+    component: DashboardPM
+  },
+  {
+    path: 'team-member/dashboard',
+    component: DashboardTM
+  },
+
 ];
